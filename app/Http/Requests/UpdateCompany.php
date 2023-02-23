466c5c0bb1a9de7528725dab7d4b3ebd9ff8a3ea
sarasi <sarasi.wc@gmail.com>
2023-02-23 08:14:02 +0530

    remove commented code lines

--- app/Http/Requests/UpdateCompany.php
+++ app/Http/Requests/UpdateCompany.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Requests;
 
-// use Illuminate\Foundation\Http\FormRequest;
-// use App\Http\Requests\StoreCompany;
 use Illuminate\Support\Arr;
 
 class UpdateCompany extends StoreCompany
